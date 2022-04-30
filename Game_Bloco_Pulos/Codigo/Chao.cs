5fcec10fa736076f1d2ea4e1cce3cbe06bfa1c72
Edmilson Hora <eshora@grupogbi.com>
2022-04-30 06:23:59 -0300

    Inicial

---
+++ Game_Bloco_Pulos/Codigo/Chao.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Bloco_Pulos.Codigo
+{
+    class Chao : EntityBase
+    {
+        public Chao(Size bounds, Graphics screen) : base(bounds, screen)
+        {
+            Altura = 100;
+            Largura = bounds.Width;
+            X = 0;
+            Y = bounds.Height - Altura;
+        }
+
+        public override void Desenha()
+        {
+            Screen.FillRectangle(Brushes.Bisque, Rect);
+        }
+    }
+}
---
+++ Game_Bloco_Pulos/Codigo/EntityBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game_Bloco_Pulos.Codigo
+{
+   abstract class EntityBase
+    {
+        public EntityBase(Size bounds, Graphics screen)
+        {
+            this.Bounds = bounds;
+            this.Screen = screen;
+        }
+
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int Altura { get; set; }
+        public int Largura { get; set; }
+        public Graphics Screen { get; set; }
+        public Size Bounds { get; set; }
+        public Rectangle Rect { get { return new Rectangle(X, Y, Largura, Altura); } }
+
+        public abstract void Desenha();
+    }
+}
